da6d7418f5be1a6191845a34a1950ddfab854ecc
oo-OO <nocognition@gmail.com>
2023-06-14 06:03:11 -0400

    Remove thegraph calls in favor of onchain data

--- scripts/merkle.js
+++ scripts/merkle.js
@@ -29,8 +29,12 @@ var callProvider = new Provider(provider);
 const lockerAddress = "0x72a19342e8F1838460eBFCCEf09F6585e32db86E";
 const lockerAbi = [{"inputs":[{"internalType":"address","name":"_user","type":"address"}],"name":"lockedBalanceOf","outputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"epochs","outputs":[{"internalType":"uint224","name":"supply","type":"uint224"},{"internalType":"uint32","name":"date","type":"uint32"}],"stateMutability":"view","type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"_user","type":"address"},{"indexed":true,"internalType":"uint256","name":"_epoch","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"_paidAmount","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"_lockedAmount","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"_boostedAmount","type":"uint256"}],"name":"Staked","type":"event"},{"inputs":[{"internalType":"uint256","name":"_epoch","type":"uint256"},{"internalType":"address","name":"_user","type":"address"}],"name":"balanceAtEpochOf","outputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"epochCount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]
 
+const delAddress = "0x469788fE6E9E9681C6ebF3bF78e7Fd26Fc015446";
+const delAbi = [{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"delegator","type":"address"},{"indexed":true,"internalType":"bytes32","name":"id","type":"bytes32"},{"indexed":true,"internalType":"address","name":"delegate","type":"address"}],"name":"ClearDelegate","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"delegator","type":"address"},{"indexed":true,"internalType":"bytes32","name":"id","type":"bytes32"},{"indexed":true,"internalType":"address","name":"delegate","type":"address"}],"name":"SetDelegate","type":"event"},{"inputs":[{"internalType":"bytes32","name":"id","type":"bytes32"}],"name":"clearDelegate","outputs":[],"stateMutability":"nonpayable","type":"function"}];
+
 const lockerMulti = new Contract(lockerAddress, lockerAbi);
 const locker = new ethers.Contract(lockerAddress, lockerAbi, provider);
+const delegation = new ethers.Contract(delAddress, delAbi, provider);
 
 // set local timezone to UTC
 process.env.TZ = 'UTC';
@@ -47,19 +51,18 @@ year = date.getFullYear();
 const epochString = "Week of "+day+" "+month+" "+year;
 
 module.exports = {
-    getUsers: async function (fullsync=false) {
+    getUsers: async function (target_block, fullsync=false) {
         // get all staked events in batches of 100000 blocks
         var stakedEvents = [];
-        var currentBlock = await provider.getBlockNumber();
-        if(fullsync) { cache.blockHeight = 14320609; cache.userBase = {}; cache.userDelegation = {}; cache.userAdjusted = {}; }
-        var startBlock = cache.blockHeight;
+        if(fullsync) { cache.addressArray = []; cache.usersBlock = 14320609; cache.delegationsBlock = 11225329; cache.userBase = {}; cache.delegations = {}; cache.userDelegation = {}; cache.userAdjusted = {}; }
+        var startBlock = cache.usersBlock;
         eventsAll = [];
-        while(startBlock < currentBlock){
+        while(startBlock < target_block){
             endBlock = startBlock + 100000;
-            if(endBlock > currentBlock){
-                endBlock = currentBlock;
+            if(endBlock > target_block){
+                endBlock = target_block;
             }
-            console.log("getting events from " + startBlock + " to " + endBlock);
+            console.log("getting lock events from " + startBlock + " to " + endBlock);
             eventsAll.push(locker.queryFilter(locker.filters.Staked(),startBlock,endBlock));
             startBlock = endBlock;
         }
@@ -70,12 +73,15 @@ module.exports = {
         var userBase = cache.userBase;
         for(var i=0; i < stakedEvents.length; i++){
             var user = ethers.utils.getAddress(stakedEvents[i].args._user);
+            if(cache.addressArray.indexOf(user) == -1){
+                cache.addressArray.push(user);
+            }
             if(userBase[user] == undefined){
                 userBase[user] = 0;
             }
         }
         console.log("Total locker addresses: "+Object.keys(userBase).length);
-        cache.blockHeight = currentBlock;
+        cache.usersBlock = target_block;
         cache.userBase = userBase;
         fs.writeFileSync('./cache.json', JSON.stringify(cache));
         console.log("User list cache updated");
@@ -99,22 +105,21 @@ module.exports = {
         console.log("current epoch: "+currentEpoch);
 
         let querySize = 1000;
-        addressArray = Object.keys(cache.userBase);
         cache.userBase = {};
-        var groups = Number( (addressArray.length/querySize) + 1).toFixed(0);
+        var groups = Number( (cache.addressArray.length/querySize) + 1).toFixed(0);
         totalVlCVX = 0;
         await Promise.all([...Array(Number(groups)).keys()].map(async i => {
             var start = i*querySize;
             var finish = i*querySize + querySize - 1;
-            if(finish >= addressArray.length){
-                finish = addressArray.length - 1;
+            if(finish >= cache.addressArray.length){
+                finish = cache.addressArray.length - 1;
             }
             console.log("get balances from " + start + " to " +finish);
             var calldata = [];
             var addresses = [];
             for(var c = start; c <= finish; c++){
-                calldata.push(lockerMulti.balanceAtEpochOf(currentEpoch, addressArray[c]));
-                addresses.push(addressArray[c]);
+                calldata.push(lockerMulti.balanceAtEpochOf(currentEpoch, cache.addressArray[c]));
+                addresses.push(cache.addressArray[c]);
             }
             //console.log(calldata);
             let balData = await callProvider.all(calldata, target_block);
@@ -128,50 +133,78 @@ module.exports = {
         }));
 
         console.log("Total vlCVX: "+totalVlCVX);
-        fs.writeFileSync('./cache.json', JSON.stringify(cache));
+        await fs.writeFileSync('./cache.json', JSON.stringify(cache));
         console.log("Balances cache updated");
         return cache.userBase;
     },
     getDelegations: async function (target_block) {
-        await callProvider.init();
-        let querySize = 600;
-        addressArray = Object.keys(cache.userBase);
-        var groups = Number( (addressArray.length/querySize) + 1).toFixed(0);
-        userDelegation = {};
-        await Promise.all([...Array(Number(groups)).keys()].map(async i => {
-            var start = i*querySize;
-            var finish = i*querySize + querySize - 1;
-            if(finish >= addressArray.length){
-                finish = addressArray.length - 1;
+        // get all staked events in batches of 100000 blocks
+        var delegateEvents = [];
+        var startBlock = cache.delegationsBlock;
+        eventsAll = [];
+        while(startBlock < target_block){
+            endBlock = startBlock + 200000;
+            if(endBlock > target_block){
+                endBlock = target_block;
             }
-            console.log("get delegations from " + start + " to " +finish);
-            var calldata = [];
-            var addresses = [];
-            for(var c = start; c <= finish; c++){
-                addresses.push(addressArray[c]);
+            console.log("getting delegation events from " + startBlock + " to " + endBlock);
+            eventsAll.push(delegation.queryFilter(delegation.filters.SetDelegate(null, "0x6376782e65746800000000000000000000000000000000000000000000000000", null),startBlock,endBlock));
+            eventsAll.push(delegation.queryFilter(delegation.filters.ClearDelegate(null, "0x6376782e65746800000000000000000000000000000000000000000000000000", null),startBlock,endBlock));
+            eventsAll.push(delegation.queryFilter(delegation.filters.SetDelegate(null, "0x0000000000000000000000000000000000000000000000000000000000000000", null),startBlock,endBlock));
+            eventsAll.push(delegation.queryFilter(delegation.filters.ClearDelegate(null, "0x0000000000000000000000000000000000000000000000000000000000000000", null),startBlock,endBlock));
+            startBlock = endBlock;
+        }
+        eventsAll = await Promise.all(eventsAll);
+        for(var i=0; i < eventsAll.length; i++){
+            delegateEvents = delegateEvents.concat(eventsAll[i]);
+        }
+        // sort delegateEvents by blockNumber then by transactionIndex
+        delegateEvents.sort(function(a, b) {
+            if(a.blockNumber == b.blockNumber){
+                return a.transactionIndex - b.transactionIndex;
+            } else {
+                return a.blockNumber - b.blockNumber;
             }
-            //console.log(calldata);
-            
-            call = await fetch("https://api.thegraph.com/subgraphs/name/snapshot-labs/snapshot", {
-                "headers": {
-                  "accept": "application/json, multipart/mixed",
-                  "content-type": "application/json",
-                  "pragma": "no-cache",
-                },
-                "body": "{\"query\":\"{\\n  delegations(where: {delegator_in: "+addslashes(JSON.stringify(addresses))+", space_in: [\\\"\\\", \\\"cvx.eth\\\"]}, first: 1000) {\\n    delegator\\n    space\\n    delegate\\n  }\\n}\"}",
-                "method": "POST"
-              });
-            response = await call.json();
-            delegations = response.data.delegations;
-            for(var d = 0; d < delegations.length; d++){
-                if(userDelegation[ethers.utils.getAddress(delegations[d].delegator)] == undefined) {
-                    userDelegation[ethers.utils.getAddress(delegations[d].delegator)] = ethers.utils.getAddress(delegations[d].delegate);
-                } else if(delegations[d].space == "cvx.eth") {
-                    userDelegation[ethers.utils.getAddress(delegations[d].delegator)] = ethers.utils.getAddress(delegations[d].delegate);
+        });
+
+        for(e in delegateEvents){
+            if(cache.userBase[ethers.utils.getAddress(delegateEvents[e].args.delegator)] != undefined && cache.addressArray.indexOf(ethers.utils.getAddress(delegateEvents[e].args.delegator)) != -1){
+                if(cache.delegations[ethers.utils.getAddress(delegateEvents[e].args.delegator)] == undefined) {
+                    cache.delegations[ethers.utils.getAddress(delegateEvents[e].args.delegator)] = {"cvx":"","global":""};
+                }
+                if(delegateEvents[e].event == "SetDelegate") {
+                    if(delegateEvents[e].args.id == "0x6376782e65746800000000000000000000000000000000000000000000000000") {
+                        cache.delegations[ethers.utils.getAddress(delegateEvents[e].args.delegator)]["cvx"] = ethers.utils.getAddress(delegateEvents[e].args.delegate);
+                    } else {
+                        cache.delegations[ethers.utils.getAddress(delegateEvents[e].args.delegator)]["global"] = ethers.utils.getAddress(delegateEvents[e].args.delegate);
+                    }
+                } else {
+                    if(delegateEvents[e].args.id == "0x6376782e65746800000000000000000000000000000000000000000000000000") {
+                        if(cache.delegations[ethers.utils.getAddress(delegateEvents[e].args.delegator)]["cvx"] == ethers.utils.getAddress(delegateEvents[e].args.delegate)) {
+                            cache.delegations[ethers.utils.getAddress(delegateEvents[e].args.delegator)]["cvx"] = "";
+                        }
+                    } else {
+                        if(cache.delegations[ethers.utils.getAddress(delegateEvents[e].args.delegator)]["global"] == ethers.utils.getAddress(delegateEvents[e].args.delegate)) {
+                            cache.delegations[ethers.utils.getAddress(delegateEvents[e].args.delegator)]["global"] = "";
+                        }
+                    }
                 }
             }
-        }));
-
+        }
+        for(user in cache.delegations) {
+            if(cache.delegations[user]["cvx"] == "" && cache.delegations[user]["global"] == "") {
+                cache.userDelegation[user] = user;
+            } else if(cache.delegations[user]["cvx"] != "") {
+                cache.userDelegation[user] = cache.delegations[user]["cvx"];
+            } else if(cache.delegations[user]["global"] != "") {
+                cache.userDelegation[user] = cache.delegations[user]["global"];
+            }
+        }
+        cache.delegationsBlock = target_block;
+        fs.writeFileSync('./cache.json', JSON.stringify(cache));
+        console.log("Delegations cache updated");
+    },
+    cleanUp: async function (target_block) {
         let zeroBalanceList = [];
         for(user in cache.userBase) {
             if(cache.userBase[user] == 0) {
@@ -190,7 +223,7 @@ module.exports = {
             var calldata = [];
             var addresses = [];
             for(var c = start; c <= finish; c++){
-                calldata.push(lockerMulti.balanceAtEpochOf(currentEpoch, zeroBalanceList[c]));
+                calldata.push(lockerMulti.lockedBalanceOf(zeroBalanceList[c]));
                 addresses.push(zeroBalanceList[c]);
             }
             //console.log(calldata);
@@ -207,8 +240,8 @@ module.exports = {
 
         userAdjusted = {};
         for(user in cache.userBase) {
-            if(userDelegation[user] == undefined) {
-                userDelegation[user] = user;
+            if(cache.userDelegation[user] == undefined) {
+                cache.userDelegation[user] = user;
             }
             if(user.toString().toLowerCase() == "undefined") {
                 console.error("undefined user" + user);
@@ -216,17 +249,17 @@ module.exports = {
             if(userAdjusted[user] == undefined) {
                 userAdjusted[user] = ethers.BigNumber.from(0);
             }
-            if(userAdjusted[userDelegation[user]] == undefined) {
-                userAdjusted[userDelegation[user]] = ethers.BigNumber.from(0);
+            if(userAdjusted[cache.userDelegation[user]] == undefined) {
+                userAdjusted[cache.userDelegation[user]] = ethers.BigNumber.from(0);
             }
-            if(userDelegation[userDelegation[user]] == undefined) {
-                userDelegation[userDelegation[user]] = userDelegation[user];
+            if(cache.userDelegation[cache.userDelegation[user]] == undefined) {
+                cache.userDelegation[cache.userDelegation[user]] = cache.userDelegation[user];
             }
-            if(userBase[userDelegation[user]] == undefined) {
-                userBase[userDelegation[user]] = 0;
+            if(cache.userBase[cache.userDelegation[user]] == undefined) {
+                cache.userBase[cache.userDelegation[user]] = 0;
             }
-            if(userDelegation[user] != user) {
-                userAdjusted[userDelegation[user]] = userAdjusted[userDelegation[user]].add(ethers.BigNumber.from(cache.userBase[user]));
+            if(cache.userDelegation[user] != user) {
+                userAdjusted[cache.userDelegation[user]] = userAdjusted[cache.userDelegation[user]].add(ethers.BigNumber.from(cache.userBase[user]));
             }
         }
         totalDelegated = ethers.BigNumber.from(0);
@@ -235,12 +268,12 @@ module.exports = {
             userAdjusted[user] = userAdjusted[user].toString();
         }
         console.log("Total delegated: "+ethers.utils.formatUnits(totalDelegated,18));
-        cache.userDelegation = userDelegation;
         cache.userAdjusted = userAdjusted;
-        fs.writeFileSync('./cache.json', JSON.stringify(cache));
-        console.log("Delegations cache updated");
-        return [userAdjusted, userDelegation];
+        await fs.writeFileSync('./cache.json', JSON.stringify(cache, null, 2));
+        console.log("Cleaned up cache");
+        return [cache.userBase, cache.userAdjusted, cache.userDelegation];
     },
+
 	createTree: async function (userBase, userAdjusted, userDelegation, target_block) {
         var elements = [];
         // sort userBase object by key
@@ -280,7 +313,7 @@ module.exports = {
             compiledProofs.users[address]["delegate"] = delegate;
 
         }
-        fs.writeFileSync('./'+epochString+'.json', JSON.stringify(compiledProofs));
+        await fs.writeFileSync('./'+epochString+'.json', JSON.stringify(compiledProofs, null, 2));
         console.log("Merkle tree written to file "+epochString+".json");
         return compiledProofs;
     }
--- scripts/run.js
+++ scripts/run.js
@@ -9,16 +9,24 @@ const dater = new EthDater(provider);
 async function main() {
     target_block = await dater.getDate(newEpoch*1000, true, false);
     target_block = target_block.block;
+    blocktime = await provider.getBlock(target_block);
+    while(blocktime.timestamp < newEpoch) {
+        target_block += 1;
+        blocktime = await provider.getBlock(target_block);
+    }
     console.log("target block: "+target_block);
+    console.log("target block time: "+blocktime.timestamp);
+    console.log("Block is "+(blocktime.timestamp-newEpoch)+" seconds into the epoch.");
     startTime = Date.now();
     // check if console argument is "--sync"
     if (process.argv[2] === '--sync') {
-        await merkle.getUsers(true);
+        await merkle.getUsers(target_block, true);
     } else {
-        await merkle.getUsers();
+        await merkle.getUsers(target_block);
     }
-    userBase = await merkle.getLockedBalances(target_block);
-    [userAdjusted, userDelegation] = await merkle.getDelegations(target_block);
+    await merkle.getLockedBalances(target_block);
+    await await merkle.getDelegations(target_block);
+    [userBase, userAdjusted, userDelegation] = await merkle.cleanUp(target_block);
     tree = await merkle.createTree(userBase, userAdjusted, userDelegation, target_block);
     endTime = Date.now();
     console.log(`\nTime elapsed: ${(endTime - startTime) / 1000} seconds`);
